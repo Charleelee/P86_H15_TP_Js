e6852a7d8a62050a5f281077d4f56a428cbfefa7
nlangloi <nmonette@hotmail.ca>
2015-04-30 00:44:51 -0400

    le push est pas mal final mais jai pas pu changer la police que ta uploader faudrais checker ca si ta 10 sec

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,9 @@
 'use strict';
 $(function() {
     smoothscroll();
+
+
+
 });
 
 // JavaScript pour le Slider
@@ -41,4 +44,93 @@ function smoothscroll() {
             scrollTop: $("#top").offset().top
         }, 2000);
     });
+}
+
+//############################form
+
+function submitform() {
+
+// nom, prenom et courriel
+
+
+    var valuenom = $('#nom').val();
+    if(valuenom == ""){
+        $('#err0').fadeIn();
+
+    }else {
+        $('#err0').fadeOut();
+    }
+    var valueprenom = $('#prenom').val();
+
+    if(valueprenom == ""){
+        $('#err1').fadeIn();
+
+    }else {
+        $('#err1').fadeOut();
+    }
+    var valuecourriel = $('#courriel').val();
+    var reg_pattern = RegExp(".+\@.+\..+");
+    if(!reg_pattern.test(valuecourriel)) {
+        $('#err2').fadeIn();
+
+    }else {
+        $('#err2').fadeOut();
+    }
+
+    // nom d'utilisateur , password et password confirm
+    var valueusername = $('#username').val();
+
+    if(valueusername == ""){
+        $('#err3').fadeIn();
+
+    }else {
+        $('#err3').fadeOut();
+    }
+    var valuepassword = $('#password').val();
+
+    if(valuepassword == ""){
+        $('#err4').fadeIn();
+
+    }else {
+        $('#err4').fadeOut();
+    }
+    var valuepassword_confirm = $('#password_confirm').val();
+
+    if(valuepassword_confirm == ""){
+        $('#err5').fadeIn();
+
+    }else if(valuepassword_confirm !== valuepassword){
+        $('#errpw2').fadeIn();
+
+    }else{
+        $('#err5').fadeOut();
+    }
+// sex age et préférences musicales
+
+    var issexselected = $("input[name='sex']").is(':checked');
+
+    if(!issexselected){
+        $('#err_msg_sex').fadeIn();
+    }else {
+        $('#err_msg_sex').fadeOut();
+    }
+    var ismusselected = $("input[name='prefmus']").is(':checked');
+
+    if(!ismusselected){
+        $('#err_msg_mus').fadeIn();
+    }else {
+        $('#err_msg_mus').fadeOut();
+    }
+    var isageselected = $("input[name='age']");
+    var age_indice = 0;
+    for ( var i = 0 ; i < isageselected.length ; i++){
+        if(isageselected[i].checked == true){
+            age_indice += 1;
+        }
+    }
+    if(age_indice == 0){
+        $('#err_msg_age').fadeIn();
+    }else {
+        $('#err_msg_age').fadeOut();
+    }
 }
